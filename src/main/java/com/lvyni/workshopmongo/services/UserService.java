40302c7a9a68042f1a02ca020be58be5b8f47bc0
Lucas Albuquerque <luucas.albuquerque23@gmail.com>
2022-05-08 11:24:42 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/lvyni/workshopmongo/services/UserService.java
+++ src/main/java/com/lvyni/workshopmongo/services/UserService.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.IncorrectUpdateSemanticsDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.lvyni.workshopmongo.domain.User;
