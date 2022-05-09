a1f379aaf0059ec27785be3a6aa50e7ce6462ed6
Lucas Albuquerque <luucas.albuquerque23@gmail.com>
2022-05-08 23:27:32 -0300

    Obtendo um post por id

---
+++ src/main/java/com/lvyni/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.lvyni.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lvyni.workshopmongo.domain.Post;
+import com.lvyni.workshopmongo.repository.PostRepository;
+import com.lvyni.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
